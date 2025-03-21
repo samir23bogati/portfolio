c74e98a57f307d55f5a1d275e1ab09e582594518
Samir Bogati <samir23bogati@gmail.com>
2025-03-21 13:02:04 +0545

    link corrected

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     <nav className="navbar">
      <img src={logo} alt="Brand Logo" className='logo' />
       <div className="desktopMenu">
-        {["intro",  "works","skills", "clients"].map((section) => (
+        {["intro", "skills", "works", "clients"].map((section) => (
           <Link 
             key={section}
             activeClass="active"
@@ -42,7 +42,7 @@ const Navbar = () => {
       <img src={Menu} alt="Menu Icon" className='mobMenu' onClick={() => setShowMenu(!showMenu)} />
 
       <div className={`navMenu ${showMenu ? 'show' : ''}`}>
-        {["intro",  "works","skills", "clients", "contact"].map((section) => (
+        {["intro","skills",  "works", "clients", "contact"].map((section) => (
           <Link 
             key={section}
             activeClass="active"
