fe8344a381407847c8d38837efeaa8c2928a1258
samir23supreme <samir@supremeitsolutions.com>
2025-12-23 14:20:04 +0545

    feat : Connect to Vercel backend

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <HelmetProvider>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </HelmetProvider>
     </React.StrictMode>
 );
